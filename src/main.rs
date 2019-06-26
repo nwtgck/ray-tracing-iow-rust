9aeb2a0e84048e662e970f50e0e40982677b0b3d
Ryo Ota <nwtgck@gmail.com>
2019-06-27 00:10:49 +0900

    [add] Create the first image in chapter 5

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,24 @@ use color3::Color3;
 use vec3::Vec3;
 use ray::Ray;
 
-fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> bool {
+fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> f32 {
     let oc: Vec3 = &r.origin - center;
     let a : f32  = r.direction.dot(&r.direction);
     let b : f32  = 2.0 * oc.dot(&r.direction);
     let c : f32  = oc.dot(&oc) - radius*radius;
     let discriminant: f32 = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn color(r: &Ray) -> Color3 {
-    if hit_sphere(&Vec3{x: 0.0, y: 0.0, z: -1.0}, 0.5, r) {
-        Color3{r: 1.0, g: 0.0, b: 0.0}
+    let t: f32 = hit_sphere(&Vec3{x: 0.0, y: 0.0, z: -1.0}, 0.5, r);
+    if t > 0.0 {
+        let n: Vec3 = (&r.point_at_parameter(t) - &Vec3{x: 0.0, y: 0.0, z: -1.0}).unit_vector();
+        &Color3{r: n.x+1.0, g: n.y+1.0, b: n.z+1.0} * 0.5
     } else {
         let unit_direction : Vec3 = r.direction.unit_vector();
         let t              : f32  = 0.5 * (unit_direction.y + 1.0);
