0494dc8a2ec6e7a7be4cb8ded0502e1cd0edc993
Ryo Ota <nwtgck@gmail.com>
2019-06-27 15:09:09 +0900

    [add] Create the image in chapter 12

--- src/main.rs
+++ src/main.rs
@@ -49,49 +49,97 @@ fn color<H: Hitable>(rng: &mut rand::rngs::StdRng, r: &Ray, hitable: &H, depth: 
     }
 }
 
+fn random_scene(rng: &mut rand::rngs::StdRng) -> impl Hitable {
+    let mut hitables: Vec<SphereHitable> = Vec::new();
+
+    hitables.push(SphereHitable {
+        center: Vec3{x: 0.0, y: -1000.0, z: 0.0},
+        radius: 1000.0,
+        material: Box::new(LambertMaterial{albedo: Color3{r: 0.5, g: 0.5, b: 0.5}})
+    });
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat: f32 = rng.gen();
+            let center: Vec3 = Vec3 {
+                x: a as f32 + 0.9 * rng.gen::<f32>(),
+                y: 0.2,
+                z: b as f32 + 0.9 * rng.gen::<f32>()
+            };
+
+            if (&center - &Vec3{x: 4.0, y: 0.2, z: 0.0}).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    hitables.push(SphereHitable {
+                        center,
+                        radius: 0.2,
+                        material: Box::new(LambertMaterial{
+                            albedo: Color3{
+                                r: rng.gen::<f32>() * rng.gen::<f32>(),
+                                g: rng.gen::<f32>() * rng.gen::<f32>(),
+                                b: rng.gen::<f32>() * rng.gen::<f32>()
+                            }
+                        })
+                    });
+                } else if choose_mat < 0.95 {
+                    hitables.push(SphereHitable {
+                        center,
+                        radius: 0.2,
+                        material: Box::new(MetalMaterial{
+                            albedo: Color3{
+                                r: 0.5 * (1.0 + rng.gen::<f32>()),
+                                g: 0.5 * (1.0 + rng.gen::<f32>()),
+                                b: 0.5 * (1.0 + rng.gen::<f32>())
+                            },
+                            f: 0.5 * rng.gen::<f32>()
+                        })
+                    });
+                } else {
+                    hitables.push(SphereHitable {
+                        center,
+                        radius: 0.2,
+                        material: Box::new(DielectricMaterial{ref_idx: 1.5})
+                    });
+                }
+            }
+        }
+    }
+
+    hitables.push(SphereHitable {
+        center: Vec3{x: 0.0, y: 1.0, z: 0.0},
+        radius: 1.0,
+        material: Box::new(DielectricMaterial{ref_idx: 1.5})
+    });
+    hitables.push(SphereHitable {
+        center: Vec3{x: -4.0, y: 1.0, z: 0.0},
+        radius: 1.0,
+        material: Box::new(LambertMaterial{albedo: Color3{r: 0.4, g: 0.2, b: 0.1}})
+    });
+    hitables.push(SphereHitable {
+        center: Vec3{x: 4.0, y: 1.0, z: 0.0},
+        radius: 1.0,
+        material: Box::new(MetalMaterial{albedo: Color3{r: 0.7, g: 0.6, b: 0.5}, f: 0.0})
+    });
+
+    ListHitable{hitables}
+}
+
 fn main() {
     let mut writer = io::BufWriter::new(io::stdout());
 
     let seed: [u8; 32] = [13; 32];
     let mut rng: rand::rngs::StdRng = rand::SeedableRng::from_seed(seed);
 
-    let nx: i32 = 200;
-    let ny: i32 = 100;
-    let ns: i32 = 100;
+    let nx: i32 = 1200;
+    let ny: i32 = 800;
+    let ns: i32 = 10;
     writer.write_all(format!("P3\n{} {}\n255\n", nx, ny).as_bytes()).unwrap();
 
-    let hitable           = ListHitable { hitables: vec![
-        SphereHitable {
-            center: Vec3{x: 0.0, y: 0.0, z: -1.0},
-            radius: 0.5,
-            material: &LambertMaterial{albedo: Color3{r: 0.1, g: 0.2, b: 0.5}}
-        },
-        SphereHitable {
-            center: Vec3{x: 0.0, y: -100.5, z: -1.0},
-            radius: 100.0,
-            material: &LambertMaterial{albedo: Color3{r: 0.8, g: 0.8, b: 0.0}}
-        },
-        SphereHitable {
-            center: Vec3{x: 1.0, y: 0.0, z: -1.0},
-            radius: 0.5,
-            material: &MetalMaterial{albedo: Color3{r: 0.8, g: 0.6, b: 0.2}, f: 0.2}
-        },
-        SphereHitable {
-            center: Vec3{x: -1.0, y: 0.0, z: -1.0},
-            radius: 0.5,
-            material: &DielectricMaterial{ref_idx: 1.5}
-        },
-        SphereHitable {
-            center: Vec3{x: -1.0, y: 0.0, z: -1.0},
-            radius: -0.45,
-            material: &DielectricMaterial{ref_idx: 1.5}
-        },
-    ]};
+    let hitable = random_scene(&mut rng);
 
-    let lookfrom: Vec3 = Vec3 {x: 3.0, y: 3.0, z: 2.0};
-    let lookat  : Vec3 = Vec3 {x: 0.0, y: 0.0, z: -1.0};
-    let focus_dist: f32 = (&lookfrom - &lookat).length();
-    let aperture  : f32  = 2.0;
+    let lookfrom: Vec3 = Vec3 {x: 13.0, y: 2.0, z: 3.0};
+    let lookat  : Vec3 = Vec3 {x: 0.0, y: 0.0, z: 0.0};
+    let focus_dist: f32 = 10.0;
+    let aperture  : f32  = 0.1;
     let camera: Camera = Camera{
         lookfrom,
         lookat,
--- src/sphere_hitable.rs
+++ src/sphere_hitable.rs
@@ -3,14 +3,15 @@ use crate::ray::Ray;
 use crate::hitable::Hitable;
 use crate::hitable::HitRecord;
 use crate::material::Material;
+use core::borrow::Borrow;
 
-pub struct SphereHitable<'a> {
+pub struct SphereHitable {
     pub center: Vec3,
     pub radius: f32,
-    pub material: &'a Material
+    pub material: Box<Material>
 }
 
-impl<'a> Hitable for SphereHitable<'a> {
+impl Hitable for SphereHitable {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
         let oc: Vec3 = &r.origin - &self.center;
         let a : f32  = r.direction.dot(&r.direction);
@@ -26,7 +27,7 @@ impl<'a> Hitable for SphereHitable<'a> {
             let t: f32  = if b1 { temp1 } else {temp2};
             let p: Vec3 = r.point_at_parameter(t);
             let normal: Vec3 = &(&p - &self.center) / self.radius;
-            Some(HitRecord{ t, p, normal, material: self.material})
+            Some(HitRecord{ t, p, normal, material: self.material.borrow()})
         } else {
             None
         }
