76a809ed2221512ad977553aac047ebaf8670d3d
Ryo Ota <nwtgck@gmail.com>
2019-06-27 14:01:19 +0900

    [add] Create the second image in chapter 10

--- src/camera.rs
+++ src/camera.rs
@@ -2,6 +2,9 @@ use crate::vec3::Vec3;
 use crate::ray::Ray;
 
 pub struct Camera {
+    pub lookfrom: Vec3,
+    pub lookat: Vec3,
+    pub vup: Vec3,
     pub vfov: f32,
     pub aspect: f32
 }
@@ -16,22 +19,32 @@ impl Camera {
     fn half_width(&self) -> f32 {
         self.aspect * self.half_height()
     }
+    fn origin(&self) -> Vec3 {
+        self.lookfrom
+    }
+    fn w(&self) -> Vec3 {
+        (&self.lookfrom - &self.lookat).unit_vector()
+    }
+    fn u(&self) -> Vec3 {
+        self.vup.cross(&self.w()).unit_vector()
+    }
+    fn v(&self) -> Vec3 {
+        self.w().cross(&self.u())
+    }
+
     fn lower_left_corner(&self) -> Vec3 {
-        Vec3{x: -self.half_width(), y: -self.half_height(), z: -1.0}
+        &(&(&self.origin() - &(&self.u() * self.half_height())) - &(&self.v() * self.half_height())) - &self.w()
     }
     fn horizontal(&self) -> Vec3 {
-        Vec3{x: 2.0 * self.half_width(), y: 0.0, z: 0.0}
+        &self.u() * (2.0 * self.half_width())
     }
     fn vertical(&self) -> Vec3 {
-        Vec3{x: 0.0, y: 2.0 * self.half_height(), z: 0.0}
+        &self.v() * (2.0 * self.half_height())
     }
-    fn origin(&self) -> Vec3 {
-        Vec3{x: 0.0, y: 0.0, z: 0.0}
-    }
-    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+    pub fn get_ray(&self, s: f32, t: f32) -> Ray {
         Ray {
             origin: self.origin(),
-            direction: &(&self.lower_left_corner() + &(&self.horizontal() * u)) + &(&self.vertical() * v)
+            direction: &(&(&self.lower_left_corner() + &(&self.horizontal() * s)) + &(&self.vertical() * t)) - &self.origin()
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -60,20 +60,41 @@ fn main() {
     let ns: i32 = 100;
     writer.write_all(format!("P3\n{} {}\n255\n", nx, ny).as_bytes()).unwrap();
 
-    let radius: f32 = (std::f32::consts::PI / 4.0).cos();
     let hitable           = ListHitable { hitables: vec![
         SphereHitable {
-            center: Vec3{x: -radius, y: 0.0, z: -1.0},
-            radius,
-            material: &LambertMaterial{albedo: Color3{r: 0.0, g: 0.0, b: 1.0}}
+            center: Vec3{x: 0.0, y: 0.0, z: -1.0},
+            radius: 0.5,
+            material: &LambertMaterial{albedo: Color3{r: 0.1, g: 0.2, b: 0.5}}
         },
         SphereHitable {
-            center: Vec3{x: radius, y: 0.0, z: -1.0},
-            radius,
-            material: &LambertMaterial{albedo: Color3{r: 1.0, g: 0.0, b: 0.0}}
+            center: Vec3{x: 0.0, y: -100.5, z: -1.0},
+            radius: 100.0,
+            material: &LambertMaterial{albedo: Color3{r: 0.8, g: 0.8, b: 0.0}}
+        },
+        SphereHitable {
+            center: Vec3{x: 1.0, y: 0.0, z: -1.0},
+            radius: 0.5,
+            material: &MetalMaterial{albedo: Color3{r: 0.8, g: 0.6, b: 0.2}, f: 0.2}
+        },
+        SphereHitable {
+            center: Vec3{x: -1.0, y: 0.0, z: -1.0},
+            radius: 0.5,
+            material: &DielectricMaterial{ref_idx: 1.5}
+        },
+        SphereHitable {
+            center: Vec3{x: -1.0, y: 0.0, z: -1.0},
+            radius: -0.45,
+            material: &DielectricMaterial{ref_idx: 1.5}
         },
     ]};
-    let camera: Camera = Camera{vfov: 90.0, aspect: nx as f32 / ny as f32};
+
+    let camera: Camera = Camera{
+        lookfrom: Vec3 {x: -2.0, y: 2.0, z: 1.0},
+        lookat: Vec3 {x: 0.0, y: 0.0, z: -1.0},
+        vup: Vec3 {x: 0.0, y: 1.0, z: 0.0},
+        vfov: 90.0,
+        aspect: nx as f32 / ny as f32
+    };
     let mut j = ny - 1;
     while j >= 0 {
         for i in 0..nx {
